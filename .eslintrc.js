056c3f94594be19413ab9f750e6380fe3312acdb
uchid <d.27.srn@gmail.com>
2020-01-27 22:15:12 +0900

    Remove prettier setting in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,22 +6,34 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:vue/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
   ],
   env: { browser: true, node: true, es6: true },
   plugins: ['@typescript-eslint'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'prettier/prettier': [
-      'error',
-      {
-        trailingComma: 'all',
-        tabWidth: 2,
-        singleQuote: true,
-        semi: true,
-      },
-    ],
+    'comma-dangle': ['error', 'always-multiline'],
+    'max-len': ['error', { 'code': 80, 'tabWidth': 2 }],
+    'quotes': ['error', 'single'],
+    "semi": "off",
+    "@typescript-eslint/semi": ["error"],
+    'vue/max-attributes-per-line': ["error", {
+      'singleline': 2,
+      'multiline': {
+         'max': 1,
+         'allowFirstLine': false
+       }
+    }],
   },
+  overrides: [
+    {
+      files: [
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)"
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ]
 };
